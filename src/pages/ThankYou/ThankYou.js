8f97ed2601e3a156dfa191989fa7fff26c341849
halil <halil_consul@list.ru>
2019-12-10 15:18:34 +0300

    change text for ThankkYou page, chage styles for cart-icon

--- src/pages/ThankYou/ThankYou.js
+++ src/pages/ThankYou/ThankYou.js
@@ -4,9 +4,10 @@ import styles from "./ThankYou.scss";
 
 const ThankYou = () => (
   <div className={styles.thankYou}>
-    <h1 className={styles.thankYou__title}>Thank You!</h1>
+    <h1 className={styles.thankYou__title}>Thank You for shopping with us!</h1>
     <div className={styles.thankYou__text}>
-      You can go to <Link to="/items"> Items Page </Link> to find something new.
+      You can go to <Link to="/items"> Items Page </Link> to find something
+      else.
     </div>
   </div>
 );
